88b782416763c6e751fdf58d0b552ccdeac35619
unknown <muktinathrajbanshi786@gmail.com>
2025-05-11 19:32:34 +0545

    How to Handle Submission in Express | GET & POST Method Explained

--- express/app.js
+++ express/app.js
@@ -5,36 +5,22 @@ import path from "path";
 
 
 const app = express();
-
-
-// console.log(import.meta.dirname);
-// console.log(import.meta.filename);
-
-
-
 // absolute path
 const staticPath = path.join(import.meta.dirname, "public");
 
-app.use("/public", express.static(staticPath));
+app.use( express.static(staticPath));
 
-app.get("/product", (req, res) => {
-    console.log(req.query);
-    res.send(`<h1> user search for Product ${req.query.page} ${req.query.limit} mobile </h1>`);
-});
+app.use(express.urlencoded({ extended: true}));
 
-app.get("/profile/:username", (req, res) => {
-    
-console.log(req.params);
-res.send(`<h1> My username is ${req.params.username} </h1>`);
+// app.get("/contact", (req, res) => {
+//     console.log(req.query);
+//     res.redirect("/");
+// });
 
-});
-
-app.get("/profile/:username/article/:slug", (req, res) => {
-console.log(req.params);
-const formatedSlug = req.params.slug.replace(/-/g, " ");
-res.send(`<h1> Article ${req.params.username} by ${formatedSlug} </h1>`);
-
-});
+app.post("/contact", (req,res) => {
+    console.log(req.body);
+    res.redirect("/");
+})
 
 app.listen(PORT, () => {
     console.log("Server is running on port 3000");
