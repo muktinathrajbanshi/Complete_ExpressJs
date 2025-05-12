b7199096d9ef4cc60f404ed136b8cf43e3b367a3
unknown <muktinathrajbanshi786@gmail.com>
2025-05-12 15:16:23 +0545

    How to Handle a 404 Error Page in Express JS

--- express/app.js
+++ express/app.js
@@ -20,7 +20,15 @@ app.use(express.urlencoded({ extended: true}));
 app.post("/contact", (req,res) => {
     console.log(req.body);
     res.redirect("/");
-})
+});
+
+app.use((req, res) => {
+    // return res.status(404).send("<h1>Page not fount </h1>");
+   return res
+   .status(404)
+   .sendFile(path.join(import.meta.dirname, "views", "404.html"));
+
+});
 
 app.listen(PORT, () => {
     console.log("Server is running on port 3000");
