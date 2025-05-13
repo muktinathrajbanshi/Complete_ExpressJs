4f55d09c98779f813dc964f4bd881d276ef60190
unknown <muktinathrajbanshi786@gmail.com>
2025-05-13 13:40:33 +0545

    Express Router Separating routes in separate modules

--- urlshortener/app.js
+++ urlshortener/app.js
@@ -1,97 +1,16 @@
-import {readFile, writeFile} from "fs/promises";
-import crypto from "crypto";
-import path from "path";
 import express from "express";
+import { shortenerRoutes } from "./routes/shortener.routes.js";
 
 const app = express();
 
-// const PORT = 3002;
 const PORT = process.env.PORT || 3000;
-const DATA_FILE = path.join("data", "links.json");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-const serveFile = async (res, filePath, contentType) => {
-    try {
-        const data = await readFile(filePath);
-        res.writeHead(200, { "Content-Type": contentType });
-        res.end(data);
-
-    } catch (error) {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("404 page not found");
-      }
-};
-
-const loadLinks = async () => {
-    try {
-        const data = await readFile(DATA_FILE, "utf-8");
-        // if (!data.trim()) return {};
-        return JSON.parse(data);
-    } catch (error) {
-        if(error.code === "ENOENT") {
-            await writeFile(DATA_FILE, JSON.stringify({}));
-            return {};
-        }
-        throw error;
-    }
-};
-
-const saveLinks = async (links) => {
-    await writeFile(DATA_FILE, JSON.stringify(links))
-};
-
-app.get("/", async (req, res) => {
-    try {
-        const file = await readFile(path.join("views", "index.html"));
-        const links = await loadLinks();
-
-        const content = file.toString().replaceAll( "{{ shortened_urls }}", Object.entries(links).map(([shortCode, url]) => `<li><a href="/${shortCode}" target="_blank">${req.host}/${shortCode}</a> - ${url}</li>`).join(""));
-
-    return res.send(content);
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Internal server error");
-    }
-});
-
-
-app.post("/", async (req, res) => {
-    try {
-        const { url, shortCode } = req.body;
-        const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-
-        const links = await loadLinks();
-
-         if (links[finalShortCode]) {
-            return res.status(400).send("Short code already exists. Please choose another.");
-         }
-
-         links[finalShortCode] = url;
-
-         await saveLinks(links);
-          return res.redirect("/");
-    } catch (error) {
-          console.error(error);
-        return res.status(500).send("Internal server error");
-    }
-});
-
-app.get("/:shortCode", async (req, res) => {
-    try {
-        const { shortCode } = req.params;
-        const links = await loadLinks();
-
-        if (!links[shortCode]) return res.status(404).send("404 error occurred");
-
-        return res.redirect(links[shortCode]);
-    } catch (err) {
-        console.error(err);
-        return res.status(500).send("Internal server error");
-    }
-});
+// express router
+// app.use(router);
+app.use(shortenerRoutes);
 
 
 app.listen(PORT, () => {
---
+++ urlshortener/routes/shortener.routes.js
@@ -0,0 +1,98 @@
+import {readFile, writeFile} from "fs/promises";
+import crypto from "crypto";
+import path from "path";
+import { Router } from "express";
+
+const router = Router();
+
+const DATA_FILE = path.join("data", "links.json");
+
+
+const loadLinks = async () => {
+    try {
+        const data = await readFile(DATA_FILE, "utf-8");
+        // if (!data.trim()) return {};
+        return JSON.parse(data);
+    } catch (error) {
+        if(error.code === "ENOENT") {
+            await writeFile(DATA_FILE, JSON.stringify({}));
+            return {};
+        }
+        throw error;
+    }
+};
+
+const serveFile = async (res, filePath, contentType) => {
+    try {
+        const data = await readFile(filePath);
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(data);
+
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 page not found");
+      }
+};
+
+const saveLinks = async (links) => {
+    await writeFile(DATA_FILE, JSON.stringify(links))
+};
+
+router.get("/", async (req, res) => {
+    try {
+        const file = await readFile(path.join("views", "index.html"));
+        const links = await loadLinks();
+
+        const content = file.toString().replaceAll( "{{ shortened_urls }}", Object.entries(links).map(([shortCode, url]) => `<li><a href="/${shortCode}" target="_blank">${req.host}/${shortCode}</a> - ${url}</li>`).join(""));
+
+    return res.send(content);
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Internal server error");
+    }
+});
+
+
+router.post("/", async (req, res) => {
+    try {
+        const { url, shortCode } = req.body;
+        const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+        const links = await loadLinks();
+
+         if (links[finalShortCode]) {
+            return res.status(400).send("Short code already exists. Please choose another.");
+         }
+
+         links[finalShortCode] = url;
+
+         await saveLinks(links);
+          return res.redirect("/");
+    } catch (error) {
+          console.error(error);
+        return res.status(500).send("Internal server error");
+    }
+});
+
+router.get("/:shortCode", async (req, res) => {
+    try {
+        const { shortCode } = req.params;
+        const links = await loadLinks();
+
+        if (!links[shortCode]) return res.status(404).send("404 error occurred");
+
+        return res.redirect(links[shortCode]);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send("Internal server error");
+    }
+});
+
+// default export
+//export default router;
+
+// Named exports
+export const shortenerRoutes = router;
+
+
