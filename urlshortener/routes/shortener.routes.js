f004619a775d36bc5c3038e4cf94d94a8ce487ce
unknown <muktinathrajbanshi786@gmail.com>
2025-05-14 17:54:30 +0545

    Controllers in Express.js: Simplify URL Shortener Project

--- urlshortener/routes/shortener.routes.js
+++ urlshortener/routes/shortener.routes.js
@@ -1,115 +1,28 @@
-import {readFile, writeFile} from "fs/promises";
-import crypto from "crypto";
-import path from "path";
 import { Router } from "express";
+import { postURLShortener, getShortenerPage, redirectToShortLink } from "../controllers/postshortner.controllers.js";
 
 const router = Router();
 
-const DATA_FILE = path.join("data", "links.json");
 
 
-const loadLinks = async () => {
-    try {
-        const data = await readFile(DATA_FILE, "utf-8");
-        // if (!data.trim()) return {};
-        return JSON.parse(data);
-    } catch (error) {
-        if(error.code === "ENOENT") {
-            await writeFile(DATA_FILE, JSON.stringify({}));
-            return {};
-        }
-        throw error;
-    }
-};
+// const serveFile = async (res, filePath, contentType) => {
+//     try {
+//         const data = await readFile(filePath);
+//         res.writeHead(200, { "Content-Type": contentType });
+//         res.end(data);
 
-const serveFile = async (res, filePath, contentType) => {
-    try {
-        const data = await readFile(filePath);
-        res.writeHead(200, { "Content-Type": contentType });
-        res.end(data);
-
-    } catch (error) {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("404 page not found");
-      }
-};
-
-const saveLinks = async (links) => {
-    await writeFile(DATA_FILE, JSON.stringify(links))
-};
-
-router.get("/report", (req, res) => {
-    const student = [
-        {
-            name: "Muktinath",
-            place: "Kathmandu",
-            sex: "Male",
-        },
-        {
-            name: "Sita",
-            place: "Pokhara",
-            sex: "Female",
-        },
-        {
-            name: "Raju",
-            place: "Simla",
-            sex: "Male",
-        },
-    ];
-        
-   return res.render("report", {student});
-});
-
-router.get("/", async (req, res) => {
-    try {
-        const file = await readFile(path.join("views", "index.html"));
-        const links = await loadLinks();
-
-        const content = file.toString().replaceAll( "{{ shortened_urls }}", Object.entries(links).map(([shortCode, url]) => `<li><a href="/${shortCode}" target="_blank">${req.host}/${shortCode}</a> - ${url}</li>`).join(""));
-
-    return res.send(content);
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Internal server error");
-    }
-});
+//     } catch (error) {
+//       res.writeHead(404, { "Content-Type": "text/plain" });
+//       res.end("404 page not found");
+//       }
+// };
 
 
-router.post("/", async (req, res) => {
-    try {
-        const { url, shortCode } = req.body;
-        const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+router.get("/", getShortenerPage);
 
-        const links = await loadLinks();
+router.post("/", postURLShortener);
 
-         if (links[finalShortCode]) {
-            return res.status(400).send("Short code already exists. Please choose another.");
-         }
-
-         links[finalShortCode] = url;
-
-         await saveLinks(links);
-          return res.redirect("/");
-    } catch (error) {
-          console.error(error);
-        return res.status(500).send("Internal server error");
-    }
-});
-
-router.get("/:shortCode", async (req, res) => {
-    try {
-        const { shortCode } = req.params;
-        const links = await loadLinks();
-
-        if (!links[shortCode]) return res.status(404).send("404 error occurred");
-
-        return res.redirect(links[shortCode]);
-    } catch (err) {
-        console.error(err);
-        return res.status(500).send("Internal server error");
-    }
-});
+router.get("/:shortCode", redirectToShortLink);
 
 // default export
 //export default router;
