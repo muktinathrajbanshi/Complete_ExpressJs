c14cac51caaaf5f3981a6e183ba05c5df119db38
unknown <muktinathrajbanshi786@gmail.com>
2025-05-17 02:33:10 +0545

    Responsive Website using HTML CSS & Express JS | Serve Static Website in Node JS(Express)

--- express_part2/src/index.js
+++ express_part2/src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import path from "path";
 const app = express();
+import { fileURLToPath } from 'url';
+
 const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // built in middleware
 
-app.use(express.static(path))
+const staticPath = path.join(__dirname, "../public");
+
+app.use(express.static(staticPath));
 
 app.get("/", (req, res) => {
     res.send("hello how are you for the express server");
