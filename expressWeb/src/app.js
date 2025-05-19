257b5095dd9d33df8f744a76293b2b60ccceefaf
unknown <muktinathrajbanshi786@gmail.com>
2025-05-19 14:47:59 +0545

    Static page is completed

--- expressWeb/src/app.js
+++ expressWeb/src/app.js
@@ -5,28 +5,31 @@ const port = process.env.PORT || 8000 ;
 
 // public static path
 const static_path = (path.join(__dirname, "../public"));
+const viewsPath = path.join(__dirname, "../views");
+
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
 
 app.use(express.static(static_path));
 
 
 // routing
-app.get("/", (req, res) => {
-    res.send("Welcome to muktinath page")
+app.get("", (req, res) => {
+    res.render("index")
 })
 
 app.get("/about", (req, res) => {
-    res.send("Welcome to about page")
+    res.render("about")
 })
 
 app.get("/weather", (req, res) => {
-    res.send("Welcome to weather page")
+    res.render("weather")
 })
 
-
-
-app.use((req, res) => {
-    res.status(404).send("404 error page oops")
-})
+// app.get("*", (req, res) => {
+//     res.status(404).send("404 error page oops")
+// })
 
 app.listen(port, () => {
     console.log(`listening to the port at ${port}`)
